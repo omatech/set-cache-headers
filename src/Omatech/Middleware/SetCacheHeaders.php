013f0d8a6c372efd47d88fe58f3a5a6b998cb3b1
Agusti Pons <apons@omatech.com>
2019-10-18 14:49:02 +0200

    carbon support

--- src/Omatech/Middleware/SetCacheHeaders.php
+++ src/Omatech/Middleware/SetCacheHeaders.php
@@ -30,11 +30,11 @@ class SetCacheHeaders
         {
           if (is_numeric($options['last_modified'])) 
           {
-            $options['last_modified']=Carbon::createFromTimestamp($options['last_modified']);
+            $options['last_modified']=\Carbon::createFromTimestamp($options['last_modified']);
           }
           else
           {
-            $options['last_modified']=Carbon::parse($options['last_modified']);						
+            $options['last_modified']=\Carbon::parse($options['last_modified']);						
           }
         }
 
